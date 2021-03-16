335b4bdf276b88fd60d651dd35821962a70120b4
satyammutha <satyammutha@gmail.com>
2021-03-16 12:25:09 +0530

    - 160321

---
+++ Assets/Script/LoadSceneScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSceneScript : MonoBehaviour
+{
+    public Button button;
+    public string scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+            button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
